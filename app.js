429b75c63f3b74bdb63293176f0ffd8e2b15c67c
Lucivando Sousa <lucivandosousa@gmail.com>
2024-02-29 17:07:43 -0300

    feat: adicionar css customizado

--- app.js
+++ app.js
@@ -43,7 +43,9 @@ const options = {
 
 const swaggerSpec = swaggerJSDoc(options)
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec, {
+  customCssUrl: "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.11.8/swagger-ui.css"
+}))
 
 app.use('/swagger-ui', express.static(swaggerDist.getAbsoluteFSPath()))
 

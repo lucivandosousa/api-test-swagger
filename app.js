4c43f0db3f2845b9c76b94afb04a968c31ba9224
Lucivando Sousa <lucivandosousa@gmail.com>
2024-02-29 15:44:32 -0300

    fix: adicionar diretorio public css

--- app.js
+++ app.js
@@ -43,7 +43,8 @@ const options = {
 
 const swaggerSpec = swaggerJSDoc(options)
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec, {customCssUrl: "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css"}))
+// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec, {customCssUrl: "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css"}))
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec, {customCssUrl: "./public/swagger-ui.css"}))
 
 // Array de produtos como exemplo de uma "base de dados".
 let produtos = [
